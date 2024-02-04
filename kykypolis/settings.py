369f50c2f279e03bb8c4131863ce28e84dfee6cf
MakarenkoVik <118835928+MakarenkoVik@users.noreply.github.com>
2024-02-04 18:02:14 +0300

    Update settings.py

--- kykypolis/settings.py
+++ kykypolis/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d06)mbo#p=x%*yhh@fy4(#19po=vfn**v@$25gt9c*5*j@&+8q'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
